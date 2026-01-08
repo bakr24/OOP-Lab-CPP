7e115d1787a07e6a79a14b0de045f3438c617d77
bakr24 <abubakarbaig2004@gmail.com>
2026-01-08 15:53:17 +0500

    Name with stars program

---
+++ Lab-01_Basics/Lab-01_Basics/Lab-01_Basics/name_using_stars.cpp
@@ -0,0 +1,14 @@
+// Topic: C++ Basics
+// Description: Simple input/output program
+// Author: Abu Bakar Baig
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"      *     * * *  *     *   * * *       *     *   *      *     ****"<<endl;
+    cout<<"     * *    *    * *     *   *    *     * *    *  *      * *    *   *"<<endl;
+    cout<<"    *****   *****  *     *   ******    *****   * *      *****   ****"<<endl;
+    cout<<"   *     *  *    * *     *   *    *   *     *  *  *    *     *  * * "<<endl;
+    cout<<"  *       * * ***  * * * *   * ****  *       * *   *  *       * *   *"<<endl;
+    return 0;
+}
