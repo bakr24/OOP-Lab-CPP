3cf88c05d01e2343e8e286aca53d9cf5a7ed2725
bakr24 <abubakarbaig2004@gmail.com>
2026-01-08 15:50:56 +0500

    Hello World program

---
+++ Lab-01_Basics/Lab-01_Basics/hello_world.cpp
@@ -0,0 +1,10 @@
+// Topic: C++ Basics
+// Description: Simple input/output program
+// Author: Abu Bakar Baig
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
